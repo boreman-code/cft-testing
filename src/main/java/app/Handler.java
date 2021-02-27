32486653b297cbbf8e4951463d893befec614a1b
bore <stepanovsound@yandex.ru>
2021-02-27 20:59:58 +0700

    Added Handler

---
+++ src/main/java/app/Handler.java
@@ -0,0 +1,56 @@
+package app;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.Closeable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Passes the paths to the input files to the readers, gets the queue list and passes this list for sorting
+ */
+public class Handler implements Closeable {
+    private Logger logger;
+    private List<FileLinesReader> readers;
+    private List<BlockingDeque<String>> queues;
+    private Mediator mediator;
+    private Sorter sorter;
+
+    public Handler(Mediator mediator, Sorter sorter) {
+        logger = LogManager.getLogger(Handler.class);
+        this.mediator = mediator;
+        this.sorter = sorter;
+
+        readers = new ArrayList<>();
+        queues = new CopyOnWriteArrayList<>();
+    }
+
+    public void processing() {
+        for (String fileName : Main.inputFileNames) {
+            FileLinesReader reader = new FileLinesReader(fileName, mediator);
+            if (reader.isReadable()) {
+                readers.add(reader);
+            }
+        }
+        if (readers.size() > 0) {
+            for (FileLinesReader reader : readers) {
+                BlockingDeque<String> queue = reader.writeLinesToQueue();
+                if (queue != null) {
+                    queues.add(queue);
+                }
+            }
+        } else {
+            logger.error("No files that can be processed");
+            System.exit(8);
+        }
+        sorter.sort(queues);
+    }
+
+    @Override
+    public void close(){
+        readers.forEach(FileLinesReader::close);
+    }
+}
